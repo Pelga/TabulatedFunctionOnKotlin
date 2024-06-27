f73a25f84e157f254f304bdcc395af15a249de6f
pelageiacherepanova <pelchel@yandex.ru>
2024-06-27 17:51:38 +0400

    Revert "Initial commit"
    This reverts commit c88e0cf7f335c5afcaa3c0e681e90e4413437d2e.
    
    (cherry picked from commit 9cec1d489726364510c42d5316219229fb419c80)

--- app/src/main/java/com/example/myapplicationkotlin/domain/entity/TabulatedFunctionEntity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplicationkotlin.domain.entity
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "tabulated_function_table_name")
-data class TabulatedFunctionEntity (@PrimaryKey() var x: Double, var y: Double)
